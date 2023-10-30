9e919c5f29d32c56526c7294115c9b7d55a2137f
nicoIosa <nicolasiosa@hotmail.com>
2023-10-30 14:44:41 -0300

    ejercicio 6 terminado

---
+++ js/ejercicio6.js
@@ -0,0 +1,11 @@
+for (let contadorExterno = 1; contadorExterno <= 30; contadorExterno++) {
+  for (
+    let contadorInterno = contadorExterno;
+    contadorInterno >= 1;
+    contadorInterno--
+  ) {
+    document.write(contadorExterno);
+  }
+  document.write("<br>");
+}
+f
